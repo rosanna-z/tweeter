1baf1225c3bc682f2e940aa43e01876aca1b2a4f
Rosanna Zhang <rosannazhang92@gmail.com>
2022-10-12 19:40:07 -0700

    Create function to make a GET request, and call renderTweets function. Add timeago to display time passed on tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,38 +1,16 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(() => {
-
   const $tweetsContainer = $('.all-tweets');
+
+  // make a request to /tweets and receive the array of tweets as JSON
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    })
+      .then((tweets) => {
+        renderTweets(tweets)
+      });
+  };
 
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -42,7 +20,6 @@ $(document).ready(() => {
       const tweetElement = createTweetElement(tweet);
       $tweetsContainer.prepend(tweetElement);
     }
-
   };
 
   const createTweetElement = function(tweetObj) {
@@ -51,7 +28,7 @@ $(document).ready(() => {
   <div class=tweet-profile><img src="${tweetObj.user.avatars}">${tweetObj.user.name}</img><span>${tweetObj.user.handle}</span></div>
   <div class="tweet-text">${tweetObj.content.text}</div>
   <footer>
-    ${tweetObj.created_at}
+    ${timeago.format(tweetObj.created_at)}
     <span>
       <i class="fa-sharp fa-solid fa-flag"></i>
       <i class="fa-solid fa-heart"></i>
@@ -63,24 +40,20 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  renderTweets(data);
-
   // listen for the form to submit
   const $form = $('#form');
-
   $form.on('submit', (event) => {
-    alert('hello')
+    // prevents browser from default action
     event.preventDefault();
-
+    // get + serialize the data from form
     const newTweet = $form.serialize();
-
+    // send info to server with POST request
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: newTweet
-    })
-    console.log(newTweet)
+    });
   });
 
-
+  loadTweets();
 });
