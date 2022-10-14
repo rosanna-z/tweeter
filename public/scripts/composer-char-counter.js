72aebf5dac35974151e4b88a746ec2415d404526
Rosanna Zhang <rosannazhang92@gmail.com>
2022-10-14 14:08:54 -0700

    Finals edits

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,16 +2,11 @@ $(document).ready(function() {
 
   // counter
   $('textarea').on('input', function() {
-    const input = $(this);
-    const text = input.val();
+    const text = $(this).val();
     const remaining = 140 - text.length;
 
-    // const $counter = $('.counter');
-    // $counter.text(remaining);
-
-    const parent = input.parent();
+    const parent = $(this).closest('form');
     const counter = parent.find('.counter');
-    console.log(counter);
     counter.text(remaining);
 
     if (remaining < 0) {
